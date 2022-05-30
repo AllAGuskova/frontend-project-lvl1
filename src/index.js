8710f961691b522f1a5dd27f2ad35c0c5bea66af
AllAGuskova <moralevaalla@gmail.com>
2022-05-30 19:29:38 +0000

    add changes index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-#!/usr/bin/env node
 import readlineSync from 'readline-sync';
-
-console.log('Welcome to the Brain Games!');
 
 const greeting = () => {
   const userName = readlineSync.question('May I have your name? ');
